2981bd97f6868f0d42059e1f5b0e854d3c7ec9ec
Sujit <mnamesujit@gmail.com>
2023-06-18 23:20:52 +0530

    added js and accessing dom elements

---
+++ script.js
@@ -0,0 +1,23 @@
+const zero = document.querySelector("#zero")
+const one = document.querySelector("#one")
+const two = document.querySelector("#two")
+const three = document.querySelector("#three")
+const four = document.querySelector("#four")
+const five = document.querySelector("#five")
+const six = document.querySelector("#six")
+const seven = document.querySelector("#seven")
+const eight = document.querySelector("#eight")
+const nine = document.querySelector("#nine")
+const clear = document.querySelector("#clear")
+const devide = document.querySelector("#devide")
+const multiply = document.querySelector("multiply")
+const add = document.querySelector("#add")
+const sub = document.querySelector("#sub")
+const equal = document.querySelector("#equal")
+const modulus = document.querySelector("#modulus")
+
+const input = document.querySelector("#input")
+const output = document.querySelector("output")
+
+
+
