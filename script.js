d991131ff80cf18c0b629bdab47ed6e3dd90e8de
Sujit <mnamesujit@gmail.com>
2023-06-20 07:42:21 +0530

    removed unnecessary logs statements

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const input = document.querySelector("#input")
 const output = document.querySelector("#output")
 const keyContainer = document.querySelector(".keyboard")
 
+// Global variables declaretion that manages states globally
 let val = []
 let operator
 let currNum = []
@@ -19,7 +20,6 @@ keyContainer.addEventListener("click", (e) => {
             currNum = []
             output.innerText = 0
             input.value = [0]
-            console.log("clearing:", val)
             return
         }
         // checking if user clicks on backspace
@@ -32,9 +32,7 @@ keyContainer.addEventListener("click", (e) => {
         // checking if user want result
         if(currObj.innerText === '=')
         {
-            console.log("calculating result")
             if(currNum!=null && currNum > 0){
-                console.log("Current Num is: ", currNum)
                 inputState(val, operator, currNum)
                 val.push(Number(currNum))
                 currNum = []
@@ -46,18 +44,14 @@ keyContainer.addEventListener("click", (e) => {
             return
         }
 
-
         // Assigning Operator into operator variable
         operator = currObj.innerText
         if(val.length <=1){
             input.value = [...val, currNum, operator].join("")
         }
-        console.log("currNum", currNum)
-        
 
         // checking if currNum is not null
         if(currNum!=null && currNum > 0){
-            console.log("Current Num is on main: ", currNum)
             val.push(Number(currNum))
             currNum = []
             return
@@ -69,17 +63,14 @@ keyContainer.addEventListener("click", (e) => {
 
         console.log(currNum)    
     }
-    console.log("global: ",val)
 })
 
 
 const calCulate = (val, operator) => {
-    console.log("calculated")
-    console.log(val)
     let firstNum = val[0]
     let nextNum = val[1]
-    console.log(operator)
     nextNum = eval(`${firstNum} ${operator} ${nextNum}`)
+
     // Updating Next Number Value
     val[0] = nextNum
     val.pop()
@@ -96,11 +87,9 @@ const inputState = (val, operator, currNum) =>{
 }
 
 const backSpace = () => {
-    console.log("backspace")
-    console.log(input.value)
+   
     let temp = input.value
     input.value = temp.slice(0, -1)
     currNum = currNum.slice(0, -1)
-    console.log(temp)
 
 }
